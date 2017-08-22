24a56e064cd2dad5caa447f0590c01879a9b62f7
Crow <calvinje@gmail.com>
2017-08-21 20:35:01 -0700

    Simplified program and restructered the query process.

--- PyBudget.py
+++ PyBudget.py
@@ -1,112 +1,70 @@
-import os
 import sys
 
-class Application():
+class Application:
     def __init__(self):
-        self.income = 0
-        self.expenses = 0
-        self.expense_list = []
-        self.expense_name = []
-        self.income_name = []
-        self.income_list = []
-        self.prompt_income()
+        """Initializes the program along with the entries variable."""
 
-    def income_ask(self):
-        add_income = input('Add income? [y/n]: ')
-        return add_income
+        self.entries = []
+        self.main()
 
-    def income_sum(self):
-        self.income = sum(self.income_list)
+    def main(self):
+        """Asks the user what kind of item they would like to add to the analysis,
+        when the user is done, the calculate function is called."""
 
-    def expense_ask(self):
-        add_expense = input('Add expense? [y/n]: ')
-        return add_expense
-
-    def expense_sum(self):
-        self.expenses = sum(self.expense_list)
-
-    def income_check(self):
-        if not self.income_list:
-            print('Please enter atleast one source of income. ')
-            self.prompt_income()
+        budget_item = input('Add Income or Expense(enter f to finish)? [-/+]: ')
+        if budget_item =='+':
+            self.prompt('income')
+        elif budget_item =='-':
+            self.prompt('expense')
         else:
-            return
+            self.calculate()
 
-    def expense_check(self):
-        if not self.expense_list:
-            print('Please enter atleast one expense. ')
-            self.prompt_expense()
-        else:
-            return
+    def prompt(self, type):
+        """Asks for user input depending on the type (Income or Expense), until
+        the user is done entering items."""
 
-    def prompt_income(self):
-        x = False
-        while not x:
-            result = self.income_ask()
-            if result == 'y':
-                income_input = int(input('Enter source of income. [Numbers Only]: '))
-                self.income_list.append(income_input)
-                income_name = input('Enter income name. [Name Only]: ')
-                self.income_name.append(income_name)
-            else:
-                self.income_check()
-                x = True
-        self.income_sum()
-        name = [name for name in self.income_name]
-        income = [income for income in self.income_list]
-        incomedict = dict(zip(name, income))
-        for k in incomedict:
-            print(k + ': ', '$' + str(incomedict[k]))
-        print('Total user income: ', '$' + str(self.income))
-        self.prompt_expense()
+        while True:
+            value = int(input('Enter %s amount (numbers only): ' % type))
+            label = input('Enter %s name: ' % type)
+            self.entries.append({
+                'value': value if type == 'income' else -value,
+                'label': label
+            })
+            more = input('Enter %s? [y/n]: ' % type)
+            if more == 'n':
+                break
+        self.main()
 
-    def prompt_expense(self):
-        x = False
-        while not x:
-            result = self.expense_ask()
-            if result == 'y':
-                expense_input = int(input('Enter expense amount. [Numbers Only]: '))
-                self.expense_list.append(expense_input)
-                expense_name = input('Enter expense name. [Name Only]: ')
-                self.expense_name.append(expense_name)
-            else:
-                self.expense_check()
-                x = True
-        self.expense_sum()
-        name = [name for name in self.expense_name]
-        expense = [income for income in self.expense_list]
-        expensedict = dict(zip(name, expense))
-        for k in expensedict:
-            print(k + ': ', '$' + str(expensedict[k]))
-        print('Total user expenses: ', '$' + str(self.expenses))
-        self.uservalue()
+    def calculate(self):
+        """Takes all values in self.entries and calculates the sum, displays
+        the appropriate response in relation to the sum. Either calls the
+        reset function or the close program function depending on user input."""
 
-    def uservalue(self):
-        valoutput = self.income - self.expenses
-        if valoutput < 0:
-            print('You are in the negative, you have a deficit of ' + '$' + str(valoutput))
-        if valoutput == 0:
-            print('You have broken even, you are spending exactly as much as you make.')
-        if valoutput > 0:
-            print('You are in the positive, you have a surplus of ' + '$' + str(valoutput))
-        another = input('Would you like to run another analysis? [y/n]: ')
+        balance = sum(entry['value'] for entry in self.entries)
+        if balance < 0:
+            print('\nYou are in the negative, you have a deficit of: ' + '$' + str(balance))
+        if balance == 0:
+            print('\nYou have broken even, you are spending exactly as much as you make.')
+        if balance> 0:
+            print('\nYou are in the positive, you have a surplus of ' + '$' + str(balance))
+        another = input('\nWould you like to run another analysis? [y/n]: ')
         if another == 'y':
             self.reset_program()
         else:
             self.close_program()
 
     def reset_program(self):
-        self.income = 0
-        self.expenses = 0
-        del self.expense_list[0:]
-        del self.expense_name[0:]
-        del self.income_name[0:]
-        del self.income_list[0:]
-        self.prompt_income()
+        """Clears self.entries and calls self.main()"""
+
+        del self.entries[0:]
+        self.main()
 
     def close_program(self):
-        print('Exiting Program.')
+        """Exits the program."""
+        
+        print('\nExiting Program.')
         sys.exit(0)
+
 
 if __name__ == '__main__':
     Application()
